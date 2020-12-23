0a99356a068d0a30362ace75067cfb75fe554933
pozithron <selcuktoklucu@gmail.com>
2020-12-22 22:58:56 -0500

    +1

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,29 +5,29 @@ import './Header.scss'
 
 const authenticatedOptions = (
   <React.Fragment>
-    <Link to="/change-password">Change Password</Link>
-    <Link to="/sign-out">Sign Out</Link>
+    <Link to="/change-password">Şifremi Değiştir</Link>
+    <Link to="/sign-out">Çıkış Yap</Link>
   </React.Fragment>
 )
 
 const unauthenticatedOptions = (
   <React.Fragment>
-    <Link to="/sign-up">Sign Up</Link>
-    <Link to="/sign-in">Sign In</Link>
+    <Link to="/sign-up">Kayıt Ol</Link>
+    <Link to="/sign-in">Oturum Aç</Link>
   </React.Fragment>
 )
 
 const alwaysOptions = (
   <React.Fragment>
-    <Link to="/">Home</Link>
+    <Link to="/">Ana Sayfa</Link>
   </React.Fragment>
 )
 
 const Header = ({ user }) => (
   <header className="main-header">
-    <h1>Uber, But For Taxis</h1>
+    <h1>Aydın Elektro Mekanik Bobinaj</h1>
     <nav>
-      { user && <span>Welcome, {user.email}</span>}
+      { user && <span>Hoşgeldiniz, {user.email}</span>}
       { user ? authenticatedOptions : unauthenticatedOptions }
       { alwaysOptions }
     </nav>
